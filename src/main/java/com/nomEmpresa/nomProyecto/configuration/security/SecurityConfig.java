05edea7b6f2946ca6edd9acec0ee284b8495d39f
agustindiaz59 <matutias10000@gmail.com>
2025-05-01 16:55:33 -0300

    Primer Commit

--- src/main/java/com/nomEmpresa/nomProyecto/configuration/security/SecurityConfig.java
+++ src/main/java/com/nomEmpresa/nomProyecto/configuration/security/SecurityConfig.java
@@ -77,10 +77,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true);
         config.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
